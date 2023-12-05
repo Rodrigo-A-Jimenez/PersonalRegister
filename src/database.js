7487f72e7d7a5dce1cf0555c5b17f48552cf980a
Rod2444 <jimenezhuancarodrigo@gmail.com>
2023-12-04 23:25:55 -0400

    avances con servidor, index.html

---
+++ src/database.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+mongoose.connect();
+
+//Falta aprender a usar mongoDB e instalar en ubuntu. <3
--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import express from "express";
+import morgan from "morgan";
+import path from "path";
 
-const app = express()
+//imports para __dirname en ES
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-app.get('/', (req, res)=> res.send('hello world'))
+import taskRoutes from "./routes/task.routes.js";
 
-app.listen(3000)
-console.log('Server is listening on port', 3000)
+const app = express();
+
+
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json())
+
+//routes
+app.use('/api/task',taskRoutes);
+
+//staticFiles
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//startingServer
+
+app.listen(app.get('port'), ()=> {
+    console.log(`Server on port ${app.get('port')}`);
+})
