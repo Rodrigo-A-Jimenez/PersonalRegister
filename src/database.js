59f96a8b6ad49314d818eeefff0ee9372bd16106
Rod2444 <jimenezhuancarodrigo@gmail.com>
2023-12-18 21:03:45 -0400

    reescritura

--- src/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const URI = 'mongodb://localhost/PersonalRegister'
-
-mongoose.connect(URI)
-    .then(db=>console.log('DB is connected'))
-    .catch(err=>console.error(err));
-
-export default mongoose;
